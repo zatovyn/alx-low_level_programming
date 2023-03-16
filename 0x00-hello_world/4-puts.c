3acbbc6dc35211105d99863f393268c1115167cf
zatovyn <zakaria.zi94@gmail.com>
2023-03-17 00:39:00 +0100

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char helloputs[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(helloputs);
+	return (0);
+}
