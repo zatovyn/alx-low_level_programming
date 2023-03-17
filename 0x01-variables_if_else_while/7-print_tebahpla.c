f0f676555167586d7365b6b58337cfdbe979c68a
zatovyn <zakaria.zi94@gmail.com>
2023-03-17 19:59:10 +0100

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
