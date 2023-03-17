367754c5f75c3723e2529048bacde68966057687
zatovyn <zakaria.zi94@gmail.com>
2023-03-17 20:15:27 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = '0';
+	char a = 'a';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	while (a <= 'f')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
