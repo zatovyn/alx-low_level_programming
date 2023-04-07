3b1db8722cf459f24424da4782aec5155f1bfead
zatovyn <zakaria.zi94@gmail.com>
2023-04-07 01:02:51 +0000

    compared two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,10 +19,10 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		if (*s1 == '\0')
-			return (0);
-		else if (*(s2 + 1) == '\0')
+		if (*(s2 + 1) == '\0')
 			return (1);
+		else if (*s1 == '\0')
+			return (0);
 		else
 			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
