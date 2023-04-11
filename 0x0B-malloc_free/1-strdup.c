c6cff843986c85e0b1750e764313423ac004904a
zatovyn <zakaria.zi94@gmail.com>
2023-04-11 19:23:08 +0000

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a given string.
+ * @str: the string to duplicate.
+ * Return: the array created.
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i = 0, len = 0;
+	char *new_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	new_str = malloc(len + 1);
+
+	if (new_str == NULL)
+		return (NULL);
+
+	while (i <= strlen(str))
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+
+	return (new_str);
+}
