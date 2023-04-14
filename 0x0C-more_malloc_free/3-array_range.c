4f31dc68284c7796f7bdfa9e8087121be5264078
zatovyn <zakaria.zi94@gmail.com>
2023-04-14 00:56:24 +0000

    create array of int

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,17 +7,17 @@
  * @max: maximum value in the array.
  * Return: pointer to the new array, NULL otherwise.
  */
- 
- int *array_range(int min, int max)
+
+int *array_range(int min, int max)
 {
 	int *p;
 	int i;
 
 	if (min > max)
 		return (NULL);
-	
+
 	p = malloc(sizeof(int) * (max - min + 1));
-	
+
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i <= max - min; i++)
